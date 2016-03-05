d11a4ae7ae21a3a867129e5169f5fd7735b67265
George Wellington <cyberhylian@gmail.com>
2016-03-05 15:42:14 +0000

    Added the first project

---
+++ web/views/FirstProject.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title> First</title>
+    </head>
+    <body>
+        <?php
+            print ("hello world, I'm writing in PHP for the first time!");
+        ?>
+    </body>
+</html>
